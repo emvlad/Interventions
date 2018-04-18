f0bffb9f3e4cefcc90cccdffe9a68f4287d6b996
Vladimir Chris <cevchris23@outlook.com>
2018-04-18 15:35:08 -0400

    création du composant accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'accueil', component:AccueilComponent},
+  {path:'', redirectTo: 'accueil', pathMatch:'full'},
+  {path:'**', redirectTo: 'accueil', pathMatch:'full'},
+
+ 
+
+        
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
